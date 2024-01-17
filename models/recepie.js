e630eb332e62c93603a3ce7fb5e8714dae903cdd
Roopa <roopareddy90@gmail.com>
2024-01-16 20:18:06 -0800

    created models for user and recepie

---
+++ models/recepie.js
@@ -0,0 +1,39 @@
+const{Model,DataTypes}=require('sequelize');
+const sequelize=require('../config/connecetion');
+
+class Recepie extends Model{}
+
+Recepie.init({
+    id:{
+        type:DataTypes.INTEGER,
+        allowNull:false,
+        primaryKey:true,
+        autoIncrement:true
+    },
+    name:{
+        type:DataTypes.STRING,
+        allowNull:false,
+    },
+    description:{
+        type:DataTypes.STRING,
+        allowNull:false,
+    },
+    comments:{
+        type:DataTypes.STRING,
+        allowNull:false,
+    },
+    user_id:{
+        type:DataTypes.INTEGER,
+        references:{
+            model:'user',
+            key:'id',
+        },
+    },
+},
+{
+    sequelize,
+    timestamps:false,
+    underscores:true,
+    freezeTableName:true,
+    modelName:'Recepie',
+});
---
+++ models/user.js
@@ -0,0 +1,59 @@
+const{Model,DataTypes}=require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize=require('../config/connecetion');
+
+class User extends Model{
+    checkPassword(loginpassword){
+        return bcrypt.compareSync(loginpassword,this.password);
+    }
+}
+
+User.init({
+    id:{
+        type:DataTypes.Integer,
+        allowNull:false,
+        primaryKey:true,
+        autoIncrement:true,
+    },
+    name:{
+        type:DataTypes.STRING,
+        allowNull:false,
+    },
+    email:{
+        type:DataTypes.STRING,
+        allowNull:false,
+        unique:true,
+        validate:{
+            isEmail:true,
+        },
+    },
+    password:{
+        type:DataTypes.STRING,
+        allowNull:false,
+        validate:{
+            len:[8],
+        },
+
+    },
+},
+{
+    hooks:{
+
+        beforeCreate: async(newUserData)=>{
+            newUserData.password= await bcrypt(newUserData.password,10);
+            return newUserData;
+        },
+
+        beforeUpdate:async(updateUserData)=>{
+            updateUserData.password=await bcrypt(updateUserData.password,10);
+            return updateUserData;
+        }
+    },
+    sequelize,
+    timestamps:false,
+    freezeTableName:true,
+    underscored:true,
+    modelName:'user',
+});
+
+model.exports=User;
