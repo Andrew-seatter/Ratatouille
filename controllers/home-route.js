9facef90216ee37f625e10e6732794db9774dff3
Andrew Seatter <135391186+Andrew-seatter@users.noreply.github.com>
2024-01-18 19:58:06 -0800

    merge conflict reso

--- controllers/home-route.js
+++ controllers/home-route.js
@@ -12,14 +12,32 @@ try {
 }
 });
 
-router.get('/home', async (req, res) => {
+router.get('/home', withAuth, async (req, res) => {
     try{
         const recipeDb = await Recipe.findAll({
             include: [
                 {
-                    model: 
-                }
-            ]
-        })
+                    model: User,
+                    attributes: ['name']
+                },
+            ],
+        });
+
+        const recipes = recipeDb.map((recipe) =>
+        recipe.get({plain: true}));
+
+        res.render('home', {
+            recipes,
+            loggedIn: req.session.loggedIn,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500),json(err);
     }
+});
+
+router.get('/user/:id', withAuth, async (req, res) => {
+    const userDb = await User.findbyPk(req.params.id);
+
+    const user = userDb.get
 })
