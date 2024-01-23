116d7cb2a42a3350cae6ef7beccda6ec0cfcfaf8
Roopa <roopareddy90@gmail.com>
2024-01-22 20:21:47 -0800

    added the comments model

--- models/index.js
+++ models/index.js
@@ -1,13 +1,35 @@
-const User=require('./user');
-const Recipes=require('./recipes');
+const User = require('./user');
+const Recipes = require('./recipes');
+const Comments=require('./comments');
 
-User.hasMany(Recipes,{
+User.hasMany(Recipes, {
     foreignKey:'user_id',
     onDelete:'CASCADE'
 });
 
 Recipes.belongsTo(User,{
     foreignKey:'user_id',
+});
+
+User.hasMany(Comments,{
+    foreignKey:'user_id',
+    onDelete:'CASCADE'
+});
+
+Comments.belongsTo(User,{
+    foreignKey:'user_id',
+
+});
+
+Recipes.hasMany(Comments,{
+    foreignKey:recipe_id,
+    onDelete:'CASCADE'
+
+});
+
+Comments.belongsTo(Recipes,{
+    foreignKey:recipe_id,
 })
 
-module.exports={User,Recipes}
+
+module.exports= {User, Recipes,Comments}
